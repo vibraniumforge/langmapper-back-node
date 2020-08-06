1b2aeb40e0f210aa9160089ad62117d881c2718c
vibraniumforge <gregm9999@gmail.com>
2020-08-05 21:15:52 -0700

    add custom routes

--- routes/api/v1/translations.js
+++ routes/api/v1/translations.js
@@ -116,4 +116,278 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+// ----------------------------------------------------
+
+// find_all_translations_by_word
+// GET api/v1/translations/search/word/:word
+// search translations by word
+// @access = public
+router.get("/search/word/:word", (req, res) => {
+  //   Translation.aggregate([
+  //     {
+  //       $lookup: {
+  //         from: "words",
+  //         localField: "word_name",
+  //         foreignField: "word_name",
+  //         as: "word_name",
+  //       },
+  //     },
+  //     { $unwind: "$word_name" },
+  //     { $match: { "language.word_name": req.params.word } },
+  //     { $project: { gender: 1 } },
+  //   ])
+  Translation.find({ word_name: req.params.word })
+    .then((words) =>
+      res.json({
+        message: `Translations of ${req.params.word} successfully returned.`,
+        success: true,
+        data: words,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// FIX ME
+// find_all_translations_by_word_gender
+// GET api/v1/translations/search/gender/:word
+// search translations by word
+// @access = public
+router.get("/search/gender/:word", (req, res) => {
+  Translation.aggregate([
+    {
+      $lookup: {
+        from: "languages",
+        localField: "language",
+        foreignField: "name",
+        as: "language",
+      },
+    },
+    { $unwind: "$language" },
+    { $match: { "translation.word_name": req.params.word } },
+  ])
+    //   below works, but does not give right info
+    //   Translation.find({ word_name: req.params.word })
+    .then((words) =>
+      res.json({
+        message: `Genders of ${req.params.word} successfully returned.`,
+        success: true,
+        data: words,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// DONE
+// find_etymology_containing
+// / GET api/v1/languages/search/etymology/:word
+// search translations by word/etymology
+// @access = public
+router.get("/search/etymology/:word", (req, res) => {
+  //   Translation.index({ etymology: "text" });
+  //   Translation.ensureIndexes({ etymology: 1 });
+  Translation.find({
+    $text: { $search: req.params.word, $caseSensitive: true },
+  })
+    .then((translations) =>
+      res.json({
+        message: `Etymologies containing ${req.params.word} successfully returned.`,
+        success: true,
+        data: translations,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// DONE
+// find_all_translations_by_macrofamily
+// GET api/v1/translations//search/macrofamily/:macrofamily
+// search translations by macrofamily
+// @access = public
+router.get("/search/macrofamily/:macrofamily", (req, res) => {
+  Translation.aggregate([
+    {
+      $lookup: {
+        from: "languages",
+        localField: "language",
+        foreignField: "name",
+        as: "language",
+      },
+    },
+    { $unwind: "$language" },
+    { $match: { "language.macrofamily": req.params.macrofamily } },
+  ])
+    .then((languages) =>
+      res.json({
+        message: `All Translations by macrofamily ${req.params.macrofamily} successfully returned.`,
+        success: true,
+        data: languages,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// find_all_translations_by_language
+// GET api/v1/translations/search/language/:language/
+// search translations by language
+// @access = public
+router.get("/search/language/:language", (req, res) => {
+  Translation.aggregate([
+    {
+      $lookup: {
+        from: "languages",
+        localField: "language",
+        foreignField: "name",
+        as: "language",
+      },
+    },
+    { $unwind: "$language" },
+    { $match: { "language.name": req.params.language } },
+  ])
+    .then((languages) =>
+      res.json({
+        message: `All Translations in ${req.params.language} successfully returned.`,
+        success: true,
+        data: languages,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// find_all_translations_by_area
+// GET api/v1/translations/search/area/:area/:word
+// search translation by area
+// @access = public
+router.get("/search/area/:area/:word", (req, res) => {
+  Translation.find({
+    word_name: req.params.word,
+    $or: [
+      { area1: req.params.area },
+      { area2: req.params.area },
+      { area3: req.params.area },
+    ],
+  })
+    //   Translation.aggregate([
+    //     {
+    //       $lookup: {
+    //         from: "languages",
+    //         localField: "language",
+    //         foreignField: "name",
+    //         as: "language",
+    //       },
+    //     },
+    //     { $unwind: "$language" },
+    //     {
+    //       $lookup: {
+    //         from: "words",
+    //         localField: "word_name",
+    //         foreignField: "word_name",
+    //         as: "word",
+    //       },
+    //     },
+    //     { $unwind: "$word" },
+    //     {
+    //       $project: {
+    //         _id: 1,
+    //         etymology: 1,
+    //         gender: 1,
+    //         link: 1,
+    //         romanization: 1,
+    //         translation: 1,
+    //         language: 1,
+    //         word_name: 1,
+    //         // area1: 1,
+    //         // area2: 1,
+    //         // area3: 1,
+    //       },
+    //     },
+    //   ])
+    .then((translations) =>
+      res.json({
+        message: `All Translations of ${req.params.word} in ${req.params.area} successfully returned.`,
+        success: true,
+        data: translations,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// DONE
+// translations_count
+// GET api/v1/translations/get/translations_count
+// get count of translations
+// @access = public
+router.get("/get/translations_count", (req, res) => {
+  Translation.countDocuments()
+    .then((count) =>
+      res.json({
+        message: "Translations count successfully returned.",
+        success: true,
+        data: count,
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
+// DONE
+// seeds
+// GET api/v1/translations/get/seeds
+// get translation seeds
+// @access = public
+router.get("/get/seeds", (req, res) => {
+  Translation.find()
+    .limit(100)
+    // .then((translations) =>
+    //   translations.map((translation) => {
+    //     // return { id: word._id, word_name: word.word_name };
+    //   })
+    // )
+    .then((translations) =>
+      res.json({
+        message: `Translations seeds successfully returned`,
+        success: true,
+        data: translations,
+      })
+    )
+    .catch((err) => {
+      console.log("err=", err);
+      res.status(404).json({ success: false, error: err });
+    });
+});
+
 module.exports = router;
+
+// [x]find_all_translations_by_word
+// [x]find_all_translations_by_gender
+// [x]find_etymology_containing
+// [x]find_all_translations_by_macrofamily
+// [x]find_all_translations_by_language
+// [x]find_all_translations_by_area
+// [x]translations_count
+// [x]seeds
+
+// # Mappers
+// []find_all_translations_by_area_europe_map
+// []find_all_translations_by_area_img
+// []find_all_genders_by_area_img
+// []find_all_etymologies_by_area_img
